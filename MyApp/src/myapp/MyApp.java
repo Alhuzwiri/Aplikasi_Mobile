80b24734609a1fa0e37c3a4c18a1402b15b7cdaa
Alhuzwiri <alfialhuzwiri@gmail.com>
2018-09-04 23:42:24 +0700

    Add files via upload

---
+++ MyApp/src/myapp/MyApp.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myapp;
+
+/**
+ *
+ * @author Alhuzwiri
+ */
+public class MyApp {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            int a,b;
+             int jumlah=5;
+            for(a=0;a<jumlah;a++){
+                for(b=1;b<=a;b++){
+                    System.out.print("+");
+                }
+                    System.out.println("");
+            }
+           
+    }   
+}
